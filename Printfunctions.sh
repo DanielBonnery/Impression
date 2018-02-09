6344088c9e4f70c6e77c5bdaee3555b2a1981324
Daniel Bonnéry <daniel.bonnery@gmail.com>
2018-02-09 10:56:34 -0500

    Create Printfunctions.sh

---
+++ Printfunctions.sh
@@ -0,0 +1,9 @@
+eeee () {  pdfbook $1.pdf;gs  -o $1.pdf  -sDEVICE=pdfwrite  -sPAPERSIZE=11x17  -dFIXEDMEDIA  -dPDFFitPage  -dCompatibilityLevel=1.4   $1-book.pdf; rm $1-book.pdf; }
+#eeee test
+
+
+ eeee () {  pdfjam --booklet 'true' --landscape --suffix book --papersize '{11in,17in}'  -- $1.pdf;}
+
+#pdfbook  test.pdf  1,4-5,9-11,31,31
+
+#gs  -o output.pdf  -sDEVICE=pdfwrite  -sPAPERSIZE=11x17  -dFIXEDMEDIA  -dPDFFitPage  -dCompatibilityLevel=1.4   test-1\,4-5\,9-11\,31\,31-book.pdf
